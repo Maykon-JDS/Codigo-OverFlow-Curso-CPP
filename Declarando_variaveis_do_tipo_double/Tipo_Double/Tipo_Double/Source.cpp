36ea7843e8ad2ed2ffc36b92a7868f104299b4ba
Maykon Dias <Maykondias2001@gmail.com>
2022-10-21 10:48:07 -0300

    Update

---
+++ Declarando_variaveis_do_tipo_double/Tipo_Double/Tipo_Double/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	/*
+	O tipo double é um tipo de ponto flutuante (Real).
+	*/
+
+	double real = 12.25;
+
+	cout << real;
+	
+	system("PAUSE >nul");
+	return 0;
+}
